205e13d5be5ef063715c48efe1e31b7f0effc0a9
Argo Zhang <argo@163.com>
2019-09-07 14:26:57 +0800

    script: add powershell files

---
+++ build.ps1
@@ -0,0 +1,19 @@
+﻿function packProj () {   
+    $branch = $($env:APPVEYOR_REPO_BRANCH)
+    $config = "Debug"
+    if ($branch -eq "master") {
+        $config = "Release"
+    }
+
+	$packVersion = $env:APPVEYOR_BUILD_VERSION
+    if ($env:APPVEYOR_REPO_BRANCH -ne "master") {
+        $packVersion += "-beta"
+    }
+
+	$projName = "src\$($env:APPVEYOR_PROJECT_NAME)"
+
+    write-host "dotnet pack $projName -c $config" -ForegroundColor Cyan
+    dotnet pack $projName -c $config -p:Version=$packVersion
+}
+
+packProj
